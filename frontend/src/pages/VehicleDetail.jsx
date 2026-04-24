0c3e3d1beb0e19943807aeeaa0e3a8bf04b27eac
Pedro Palheta <palhetapedro11@gmail.com>
2026-04-24 16:44:00 -0300

    Fix again:useAdmin duplicado

--- frontend/src/pages/VehicleDetail.jsx
+++ frontend/src/pages/VehicleDetail.jsx
@@ -17,7 +17,6 @@ export default function VehicleDetail({ user }) {
 
   // URL da API (Ajustada para o seu Northflank)
   const API_URL = import.meta.env.VITE_API_URL || "https://palheta--auto-ultimatebackend--jyc2t58tq8fd.code.run";
-  const isAdmin = useAdmin();
   const bgUrl = "https://images.unsplash.com/photo-1518306727298-4c17e1bf6942?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   useEffect(() => {
