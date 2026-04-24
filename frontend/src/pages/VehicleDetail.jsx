4b86e1bbd7c49cf0fa29deb4f1226fa6f1bad902
Pedro Palheta <palhetapedro11@gmail.com>
2026-04-24 16:40:09 -0300

    Fix:isAdmin duplicado

--- frontend/src/pages/VehicleDetail.jsx
+++ frontend/src/pages/VehicleDetail.jsx
@@ -6,19 +6,18 @@ import axios from 'axios';
 import { useAdmin } from '../hooks/useAdmin';
 
 export default function VehicleDetail({ user }) {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const isAdmin = useAdmin();
   const navigate = useNavigate();
   const [vehicle, setVehicle] = useState(null);
   const [loading, setLoading] = useState(true);
   const [isAdded, setIsAdded] = useState(false);
   const [deleting, setDeleting] = useState(false);
- 
+
 
   // URL da API (Ajustada para o seu Northflank)
   const API_URL = import.meta.env.VITE_API_URL || "https://palheta--auto-ultimatebackend--jyc2t58tq8fd.code.run";
-
-  const isAdmin = user?.email === "palhetapedro@gmail.com";
+  const isAdmin = useAdmin();
   const bgUrl = "https://images.unsplash.com/photo-1518306727298-4c17e1bf6942?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   useEffect(() => {
