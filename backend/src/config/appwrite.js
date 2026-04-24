e10a0d9e524fcfe23a4d996e791f0e5dc11fcb70
Pedro Palheta <palhetapedro11@gmail.com>
2026-04-24 17:13:10 -0300

    fix:Client duplicado

--- backend/src/config/appwrite.js
+++ backend/src/config/appwrite.js
@@ -1,9 +1,6 @@
 const { Client, Databases, Storage } = require('node-appwrite');
 require('dotenv').config();
 
-const { Client, Databases, Storage, ID } = require('node-appwrite');
-require('dotenv').config();
-
 const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
     .setProject(process.env.APPWRITE_PROJECT_ID)
